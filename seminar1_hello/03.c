0b408bcf9d7cc2026f658cb85cd2b1aaaca97d24
Airat Avkhadeev <67315811+airavil@users.noreply.github.com>
2026-09-09 10:37:55 +0300

    kamit

---
+++ seminar1_hello/03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%i", &a);
+    if (a >= 0 and a <= 20) or (a > 100 and a < 200)
+        printf("Yes");
+    else
+        printf("No");
+}
