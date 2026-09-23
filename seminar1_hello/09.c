907b4473d6bce906c6bac6f05756a6beda9a6e76
Airat Avkhadeev <67315811+airavil@users.noreply.github.com>
2026-09-23 12:22:51 +0300

    eho

--- seminar1_hello/09.c
+++ seminar1_hello/09.c
@@ -5,10 +5,10 @@ int main()
     scanf("%i", &n);
     int a[n];
     for (int i = 0; i < n; ++i)
-        {
+    {
         scanf("%i", &a[i]);
         printf("%i ", a[i]);
-        }
+    }
     for (int i = 0; i < n; ++i)
         printf("%i ", a[i]);
     printf("\n");
